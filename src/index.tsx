a330de68e0230606e443dea6ce723314e885b751
Itay Golbary <itay@golbary.io>
2021-06-04 18:55:47 +0300

    restructured imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
+
 import App from './App';
 import { store } from './app/store';
-import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
