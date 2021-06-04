39e06c229d408e8e4d167172347655b2cd298ec6
Itay Golbary <itay@golbary.io>
2021-06-05 00:06:58 +0300

    added Selector

---
+++ src/components/Selector.tsx
@@ -0,0 +1,47 @@
+import React, { FC, useCallback } from "react";
+import {
+  Box,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select as MaterialSelect,
+} from "@material-ui/core";
+
+interface Props {
+  label: string;
+  value?: string;
+  options: string[];
+  onChange: (newValue: string) => void;
+}
+
+const Selector: FC<Props> = ({ value, label, options, onChange }) => {
+  const handleChange = useCallback(
+    (event) => {
+      onChange(event.target.value);
+    },
+    [onChange]
+  );
+
+  return (
+    <Box>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+        <MaterialSelect
+          labelId={label}
+          id="demo-simple-select"
+          value={value}
+          label={label}
+          onChange={handleChange}
+        >
+          {options.map((category) => (
+            <MenuItem key={category} value={category}>
+              {category}
+            </MenuItem>
+          ))}
+        </MaterialSelect>
+      </FormControl>
+    </Box>
+  );
+};
+
+export { Selector };
