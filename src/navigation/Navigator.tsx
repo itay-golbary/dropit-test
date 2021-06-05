974059edfffea9c2e0bcf0c3d5d88004a92b2e02
Itay Golbary <itay@golbary.io>
2021-06-05 11:53:54 +0300

    removed product page

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Product from "../pages/Product";
+// import Product from "../pages/Product";
 import Home from "../pages/Home";
 import Catalog from "../pages/Catalog";
 import Cart from "../pages/Cart";
@@ -14,7 +14,7 @@ const Navigator: FC<Props> = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/catalog" component={Catalog} />
       <Route exact path="/cart" component={Cart} />
-      <Route path="/products/:id" component={Product} />
+      {/*<Route path="/products/:id" component={Product} />*/}
     </Switch>
   </Router>
 );
